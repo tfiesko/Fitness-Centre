118cd3f804d76cbffd9a202c79d0c19ec7d43baf
Anton Tarasenko <report641@gmail.com>
2023-05-26 17:16:42 +0700

    Блок тренеры

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -2,3 +2,13 @@
 import './vendor/swiper';
 import './vendor/focus-visible-polyfill';
 
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  slidesPerView: 'auto',
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});

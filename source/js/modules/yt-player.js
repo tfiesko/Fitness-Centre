aea27ec8d2ca10a7834bfec370678c2526a358f4
Anton Tarasenko <report641@gmail.com>
2023-05-31 16:34:58 +0700

    правки

--- source/js/modules/yt-player.js
+++ source/js/modules/yt-player.js
@@ -3,25 +3,29 @@ const playVideoButton = document.querySelector('[data-video-button]');
 const videoBlock = document.querySelector('[data-video]');
 
 let player;
+
 export function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    videoId: '9TZXsZItgdw',
-    playerVars: {'autoplay': 0, 'controls': 0, 'modestbranding': 1, 'showinfo': 0},
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange,
-    },
-  });
+  if (videoBlock) {
+    player = new YT.Player('player', {
+      videoId: '9TZXsZItgdw',
+      playerVars: {'autoplay': 0, 'controls': 0, 'modestbranding': 1, 'showinfo': 0},
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange,
+      },
+    });
+  }
 }
 
 function onPlayerReady(event) {
   event.target.playVideo();
 }
 
-playVideoButton.addEventListener('click', ()=>{
-  player.playVideo();
-});
-
+if (playVideoButton) {
+  playVideoButton.addEventListener('click', ()=>{
+    player.playVideo();
+  });
+}
 
 function onPlayerStateChange(event) {
   if (event.data === YT.PlayerState.ENDED) {
--- source/js/vendor.js
+++ source/js/vendor.js
@@ -2,24 +2,28 @@
 import './vendor/swiper';
 import './vendor/focus-visible-polyfill';
 
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  slidesPerView: 'auto',
-  loop: true,
+if (document.querySelector('.swiper')) {
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerView: 'auto',
+    loop: true,
 
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+}
 
-const swiperReviews = new Swiper('.swiper-reviews', {
-  // Optional parameters
-  spaceBetween: 30,
-  slidesPerView: 1,
+if (document.querySelector('.swiper-reviews')) {
+  const swiperReviews = new Swiper('.swiper-reviews', {
+    // Optional parameters
+    spaceBetween: 30,
+    slidesPerView: 1,
 
-  navigation: {
-    nextEl: '.swiper-reviews-button-next',
-    prevEl: '.swiper-reviews-button-prev',
-  },
-});
+    navigation: {
+      nextEl: '.swiper-reviews-button-next',
+      prevEl: '.swiper-reviews-button-prev',
+    },
+  });
+}
